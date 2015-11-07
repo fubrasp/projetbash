fc639e259bf5d3847132b2e365e47aeebc5f16f8
Romain ruez <rer>
2015-11-07 17:06:44 +0100

    ajout des argument de notre script , tapez -h apres script

--- script.sh
+++ script.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
-echo "test"
+function usage(){
+    printf "Utilisation du script :\n"
+    printf "\t--conf                   : lance le backup  \n"
+    printf "\t-h                       : affiche ce message.\n"
+}
+ 
+if [ $# -eq 0 ]
+then
+    usage
+fi
+ 
+function showHome(){
+   echo "vous avez ecrit" $1
+}
+ 
+OPTS=$( getopt -o h -l conf: -- "$@" )
+if [ $? != 0 ]
+then
+    exit 1
+fi
+eval set -- "$OPTS"
+ 
+while true ; do
+    case "$1" in
+        -h) usage;
+            exit 0;;
+        --conf) showHome $2;
+                shift 2;;
+        --) shift; break;;
+	
+    esac
+done
+ 
+exit 0
