16efe6c0bc5afc0abc09af86a482a0d9b545408a
BERTRAND Guillaume <guillaume5524@hotmail.fr>
2015-11-08 11:18:12 +0100

    fichier de conf simple

--- script.sh
+++ script.sh
@@ -7,10 +7,12 @@
 		
 #a titre de test
 
-#fonction  dans le fichier test2.cfg
-consoleLog
-
 #fichier_conf_nom=""
+
+#IMPORTS
+#on doit dabord utiliser la fonction aui partage ses variables avec le bash courant
+importNoms
+fich_conf_type2="${dossiers_de_sauvegarde[@]}"
 
 function usage(){
     printf "Utilisation du script :\n"
@@ -19,6 +21,31 @@ function usage(){
     printf "\t-h                       : affiche ce message.\n"
 }
  
+
+function recherche_copie(){
+	backup="backup_"
+	current_hour=$(date +%Y%H%M%S)
+	separateur="/"
+	full_path=$PWD$separateur$backup$current_hour
+
+	for fichier in $@
+	do
+		result=$(find /home -name "$fichier")
+		if [ -z $result ]
+		then
+			echo "$fichier n'existe pas"
+		else
+			echo "$fichier trouve:"
+			echo $result
+			#$full_path reste le meme dans la boucle!
+			cp -R $fichier $full_path
+		fi
+	done
+}
+
+
+
+
 if [ $# -eq 0 ]
 then
     usage
@@ -32,14 +59,15 @@ function backup(){
 	mkdir "$1"
     fi  
     echo "recupération des dossiers de $fichier_conf_nom"
-    while read line  
-    do   
-	echo -e " copie de $line"
+#    while read line  
+#    do   
+#	echo -e " copie de $line"
 	#find /home -type d -name $line -print
 	#marche pas
-    done < $fichier_conf_nom
+#    done < $fichier_conf_nom
     
-    echo "le backup est dans :" $1
+#    echo "le backup est dans :" $1
+recherche_copie $fich_conf_type2 
 }
 
 function recup(){
