0956727a4ee3b6707fe11a3441cb316f45277892
bertran95u <bertran95u@lenovo-pc.home>
2015-12-05 11:37:57 +0100

    traitement des cas par defaut et fichier de conf vide, possibilite de configurer en double commandes

--- script.sh
+++ script.sh
@@ -1,7 +1,46 @@
 #!/bin/bash
 
+#les fonctions doivent etre avant leurs appels
+function confexample(){
+    echo ""
+    fichier_conf_example="conf_exemple.txt"
+    echo "###un fichier type###"
+    echo "#juste de dossier listes"
+    echo "#debut du fichier de conf"
+    echo "dossier1"
+    echo "dossier2"
+    echo "dossier3"
+    echo "#fin du fichier de conf"
+    printf "dossier1\ndossier2\ndossier3" > $fichier_conf_example
+    echo "un exemple a ete cree dans $fichier_conf_example"
+    echo ""
+}
+
+
 #nom du fichier de configuration passe en parametre de, apres --backupdir
-fichier_conf_nom=$3
+#argument qui n'est pas passe, cela veut dire qu'on peut faire la commande en 2 fois, passage par defaut
+if [ "$3" == "" ]
+then
+    echo "Pas d'argument de conf, utilisation du passage par defaut"
+    fichier_conf_nom="test2.txt" 
+fi
+
+#test de l'existance du parametre passe, le cas echeant passage par defaut
+#if [ -e $fichiervide ]; then
+#  echo "fichier existe"
+#fi
+
+#fichier non vide!
+if [ -s $2 ]
+then
+    fichier_conf_nom=$2
+#fichier de conf vide!
+else
+    echo "fichier de conf $2 vide!!"
+    echo "Recommencez avec un bon fichier de conf"
+    confexample
+    exit 1
+fi
 
 #verification pour le backup
 a=""
